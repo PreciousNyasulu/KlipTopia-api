159625fed13edfa10229fb2935478007e37bdf26
preciousnyasulu <preciousnyasulu441@gmail.com>
2023-12-04 18:52:34 +0200

    add clipboard controller

---
+++ internal/service/controller/clipboard_controller.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"kliptopia-api/internal/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Copy(c *gin.Context){
+	var clipCopyData models.QueueMessage
+	
+	if err := c.ShouldBindJSON(&clipCopyData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	
+	if err := validate.Struct(&clipCopyData); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	
+}
