2ef491eedd4b8afd90c77ee140a62fc54ebcadaf
preciousnyasulu <preciousnyasulu441@gmail.com>
2023-12-20 17:37:55 +0200

    added validation for clipboard model added clipboard controller

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -18,6 +18,7 @@ func Start(){
 	// auth routes
 	r.POST("/api/auth/register",controller.CreateUserHandler)
 	r.POST("/api/auth/login",controller.LoginHandler)
+	r.POST("/api/clipboard/copy")
 
 	r.Run(":9000")
 }
