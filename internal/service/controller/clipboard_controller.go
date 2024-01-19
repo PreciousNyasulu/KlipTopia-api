6de79ee6d9796d0c2326406f2b5dc896d1747627
Precious Nyasulu <preciousnyasulu441@gmail.com>
2024-01-19 12:42:48 +0200

    feat(controller): added clipboard copy controller function :)

--- internal/service/controller/clipboard_controller.go
+++ internal/service/controller/clipboard_controller.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
+	"fmt"
 	"kliptopia-api/internal/models"
 	"net/http"
+	"time"
+
+	mr_rabbit "kliptopia-api/internal/rabbitmq_processes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,16 +14,19 @@ import (
 func Copy(c *gin.Context) {
 	var clipCopyData models.QueueMessage
 
+	clipCopyData.CopiedAt = time.Now()
 	if err := c.ShouldBindJSON(&clipCopyData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest,gin.H{"message": fmt.Sprintf("Failed to parse request message, %s", err.Error())})
 		return
 	}
 
 	if err := validate.Struct(&clipCopyData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 
-	
-
+	if err := mr_rabbit.PushMessageToQueue("", &clipCopyData); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
 }
